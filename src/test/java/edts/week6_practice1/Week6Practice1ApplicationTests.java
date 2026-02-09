70b2ea9ade117d02c9f5f3889075f701010e6791
muhammaddafa <ardidafa21@gmail.com>
2026-02-09 11:37:41 +0700

    init(chore): Init a project for Task Week 6 - Practice 1

---
+++ src/test/java/edts/week6_practice1/Week6Practice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edts.week6_practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6Practice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
