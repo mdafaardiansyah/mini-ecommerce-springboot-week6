63ed45f65896475c56dd8ae3f0460db6914081d1
muhammaddafa <ardidafa21@gmail.com>
2026-02-10 10:37:12 +0700

    docs(entity): add javadocs inside Category Entity

--- src/main/java/edts/week6_practice1/entity/Category.java
+++ src/main/java/edts/week6_practice1/entity/Category.java
@@ -9,6 +9,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Category.
+ */
 @Entity
 @Table(name = "categories")
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
@@ -39,77 +42,162 @@ public class Category {
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Product> products = new ArrayList<>();
 
+    /**
+     * On create.
+     */
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
 
+    /**
+     * On update.
+     */
     @PreUpdate
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
 
+    /**
+     * Instantiates a new Category.
+     */
     public Category() {
     }
 
+    /**
+     * Instantiates a new Category.
+     *
+     * @param name        the name
+     * @param description the description
+     */
     public Category(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets is deleted.
+     *
+     * @return the is deleted
+     */
     public Boolean getIsDeleted() {
         return isDeleted;
     }
 
+    /**
+     * Sets is deleted.
+     *
+     * @param isDeleted the is deleted
+     */
     public void setIsDeleted(Boolean isDeleted) {
         this.isDeleted = isDeleted;
     }
 
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Sets created at.
+     *
+     * @param createdAt the created at
+     */
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Gets updated at.
+     *
+     * @return the updated at
+     */
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
 
+    /**
+     * Sets updated at.
+     *
+     * @param updatedAt the updated at
+     */
     public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
 
+    /**
+     * Gets products.
+     *
+     * @return the products
+     */
     public List<Product> getProducts() {
         return products;
     }
 
+    /**
+     * Sets products.
+     *
+     * @param products the products
+     */
     public void setProducts(List<Product> products) {
         this.products = products;
     }
