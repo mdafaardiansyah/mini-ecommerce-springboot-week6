9d695ddfd4880f21e6286a9110a74808d23fcfa9
muhammaddafa <ardidafa21@gmail.com>
2026-02-10 10:25:37 +0700

    feat(exception): Create a Business Exception

---
+++ src/main/java/edts/week6_practice1/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package edts.week6_practice1.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
